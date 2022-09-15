5a9961edc21997e54ee6e7759841245618a2de24
Hamafac <borysenko777@gmail.com>
2022-09-15 23:13:04 +0300

    sending easy props with TS & interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+import React from 'react';
+import Card, { CardVariant } from './components/Card';
+
+
+const App = () => {
+   return (
+      <div>
+         <Card
+            variant={CardVariant.primary}
+            width='200px'
+            height='200px' >
+            <button>Кнопка</button>
+            <div>drou eba</div>
+         </Card>
+      </div>
+   );
+};
 
 export default App;
