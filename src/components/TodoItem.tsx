011653aa18fd8e6086182ac883e716f610a25eff
Hamafac <borysenko777@gmail.com>
2022-09-17 23:20:34 +0300

    add List with types, example todos

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,19 @@
+
+
+import React, { FC } from 'react';
+import { ITodo } from '../types/types';
+
+interface TodoItemProps {
+   todo: ITodo;
+}
+
+const TodoItem: FC<TodoItemProps> = ({ todo }) => {
+   return (
+      <div style={{ padding: 15, border: '2px solid blue' }}>
+         <input type="checkbox" checked={todo.completed} />
+         {todo.id}. {todo.title}
+      </div>
+   );
+};
+
+export default TodoItem;
